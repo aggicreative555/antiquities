c39f4de21bb4c11f6d2506e6051f2e37cd168349
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-02-19 21:01:23 +0100

    feat(login): add login functionality
    added onLogin function,
    added login function
    added intit function
    save user
    save accessToken
    #9

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,52 @@
-export async function onLogin(event) {}
+import { login } from '../../api/auth/login';
+
+/**
+ * Handles the login form submission by passing data to the login function.
+ *
+ * Disabled the submit button to prevent key spamming and multiple submissions.
+ *
+ * @async
+ * @function onLogin
+ * @throws {Error} If login form is missing in the DOM.
+ */
+
+export function onLogin() {
+  const form = document.querySelector('#loginForm');
+
+  if (form) {
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const submitButton = form.querySelector('button[type="submit"]');
+
+      if (!submitButton) {
+        console.error('Submit button not found', error);
+        return;
+      }
+
+      submitButton.disabled = true;
+      submitButton.textContent = 'Loggin in...';
+
+      const formData = new FormData(form);
+      const profile = Object.fromEntries(formData);
+
+      try {
+        await login(profile);
+        alert('Successful Login!');
+        // setTimeout(() => {
+        //   window.location.href = "/post/";
+        // }, 5);
+        return false;
+      } catch (error) {
+        console.error('Error logging in user', error);
+        alert('An error occurred while loggin in. Please try again.');
+      } finally {
+        submitButton.disabled = false;
+        submitButton.textContent = 'Login';
+        form.reset();
+      }
+    });
+  } else {
+    console.error('Error submitting login information');
+  }
+}
