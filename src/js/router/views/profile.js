b430d57e2cc9f955642e210fafe91b0b7f0d3648
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-02-23 21:30:37 +0100

    feat(profile): add initalizeProfilePage
    Makes sure the correct functions display on the profile page #15 #17

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,3 +1,31 @@
-import { authGuard } from '../../utilities/authGuard';
+import { authGuard, load } from '../../utilities/authGuard';
+import { renderProfile } from '../../ui/profile/profileTemplate';
 
-authGuard();
+async function initializeProfilePage() {
+  const profileContainer = document.querySelector('#profileContainer');
+  const editProfileContainer = document.querySelector('#editProfileContainer');
+
+  try {
+    authGuard();
+
+    const storedUser = load('user');
+
+    if (!storedUser?.name) {
+      console.error('Error: No logged-in user found.');
+      window.location.href = '/login';
+      return;
+    }
+
+    await renderProfile(storedUser.name, profileContainer);
+  } catch (error) {
+    console.error('Error setting up authGuard', error);
+  }
+
+  //   try {
+  //     setLogoutListener();
+  //   } catch (error) {
+  //     console.error("Error setting up logout", error);
+  //   }
+}
+
+initializeProfilePage();
