9acfcb9d83ddebeacf95b3e0a7fccbdefc938971
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-02-23 21:32:37 +0100

    feat(profile): add readProfile API 'POST'
    #15 #16

--- src/js/api/profile/read.js
+++ src/js/api/profile/read.js
@@ -1,3 +1,32 @@
-export async function readProfile(username) {}
+import { API_AUCTION_PROFILES } from '../constants';
+import { getHeaders } from '../headers';
+/**
+ * Reads a logged in profile.
+ *
+ * @async
+ * @function readProfile
+ * @param {string|number} username - The username of the profile to read.
+ * @returns {Promise<object>} The response data.
+ * @throws {Error} If the API request fails or the id is absent.
+ *
+ * @example
+ * const readProfile = await readProfile('user_1234');
+ * console.log(readProfile) // Outputs specific profile data.
+ */
+
+export async function readProfile(username) {
+  const response = await getHeaders(`${API_AUCTION_PROFILES}/${username}`, {
+    method: 'GET',
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to read profile with username ${username}: ${response.statusText}`,
+    );
+  }
+
+  const readProfile = await response.json();
+  return readProfile;
+}
 
 export async function readProfiles(limit, page) {}
