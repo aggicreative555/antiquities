34533a6279992ea47b6b8c7db0201ee47da65606
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-02-27 16:00:12 +0100

    feat(logout): add logout logic
    add logout logic, removing accesstoken and user from lcoalstroage and session storage, redirecting to login page.
    #19
    needs to be added globally to app.js

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,1 +1,47 @@
-export function onLogout() {}
+import { remove } from '../../utilities/authGuard';
+
+/**
+ * Logs out the user by removing the access token from storage and redirecting to the login page.
+ *
+ * @function onLogout
+ * @throws {Error} If accessToken or user is missing or cannot be removed.
+ *
+ */
+
+export function onLogout() {
+  try {
+    remove('accessToken');
+    sessionStorage.removeItem('accessToken');
+    remove('user');
+    sessionStorage.removeItem('user');
+
+    alert('You are now logged out.');
+    window.location.href = '/login/';
+  } catch (error) {
+    console.error('Logout unsuccessful. Refresh the page and try again.');
+    throw error;
+  }
+}
+
+/**
+ * Attaches a click event listener to the logout button to handle user logout.
+ *
+ * @function setLogoutListener
+ * @throws {Error} If the logout button is not found in the DOM.
+ *
+ */
+
+export function setLogoutListener() {
+  try {
+    const logoutButton = document.querySelector('#logoutButton');
+
+    if (logoutButton) {
+      logoutButton.addEventListener('click', onLogout);
+    } else {
+      throw new Error('Logout button not found');
+    }
+  } catch (error) {
+    console.error('Logout listener not working.');
+    throw error;
+  }
+}
