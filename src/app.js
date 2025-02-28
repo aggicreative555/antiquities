fa59130b9e6b6635ed2fe160a35c40dafb132f7e
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-02-28 19:50:55 +0100

    chore(sass): update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 
 import router from './js/router';
+import { setLogoutListener } from './js/ui/global/logout';
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutButton = document.getElementById('logoutButton');
+
+    if (logoutButton) {
+        setLogoutListener();
+    } else {
+        console.error("Error setting up logout listener:");
+    }
+});
+
 
 await router(window.location.pathname);
