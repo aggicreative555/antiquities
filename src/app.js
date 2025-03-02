c29f0f8dafc071a0059d4655286a6202ae54b717
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-03-02 10:27:28 +0100

    feat(footer): dynamic footer template

--- src/app.js
+++ src/app.js
@@ -4,16 +4,24 @@ import './scss/styles.scss';
 
 import router from './js/router';
 import { navTemplate } from './js/ui/global/nav';
+import { footerTemplate } from './js/ui/global/footer';
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById('header');
+    const footer = document.getElementById('footer');
 
     if (header) {
         navTemplate();
     } else {
         console.error("Header element not found or error setting up nav");
+    }
+
+    if (footer) {
+        footerTemplate();
+    } else {
+        console.error("Footer element not found or error setting up footer");
     }
 });
 
